094ced81dc06b43d2d2a838c07824726a7a12885
Dell <Dell@192.168.0.5>
2020-08-08 16:54:15 +0530

    commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,19 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Demo {
+	
+	@Test
+	public void runTest()
+	{
+		Reporter.log("Successfully running",true);
+		Reporter.log("Successfully running",true);
+		Reporter.log("Successfully running",true);
+	}
+
+}
+
+
