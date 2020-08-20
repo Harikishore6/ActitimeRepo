f9c78a72e5044ed68e2e6d1e10fbade2ae5d0c9f
Dell <Dell@192.168.0.3>
2020-08-20 18:04:34 +0530

    adding commit

--- Actitime/src/test/java/com/actitime/timetrack/Demo.java
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -12,6 +12,10 @@ public class Demo {
 		Reporter.log("Successfully running",true);
 		Reporter.log("Successfully running",true);
 		Reporter.log("Successfully running",true);
+		Reporter.log("Successfully running",true);
+		
+
+
 	}
 
 }
